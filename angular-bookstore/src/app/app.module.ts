3b05e76887523b8ad815500a8f602f742f7f18f2
Developers Vandate <developers.vandate@gmail.com>
2020-05-18 11:40:17 +0700

    Create model, class, and modify service class for bookCategory

--- angular-bookstore/src/app/app.module.ts
+++ angular-bookstore/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BookListComponent } from './components/book-list/book-list.component';
 import { BookService } from './services/book.service';
+import { BookCategoryComponent } from './components/book-category/book-category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookListComponent
+    BookListComponent,
+    BookCategoryComponent
   ],
   imports: [
     BrowserModule,
